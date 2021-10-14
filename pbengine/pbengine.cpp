2e314bd7f4e82bafeee89cdd8436794090fdc4b4
Luca Apolzan <jaguar3217@gmail.com>
2021-10-14 22:34:20 +0300

    Thread in Class (segfault)

--- pbengine/pbengine.cpp
+++ pbengine/pbengine.cpp
@@ -1,6 +1,7 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 #include <iostream>
+#include <pbengine.hpp>
 
 bool initEngine(sf::RenderWindow& window) {
     sf::Font font;
@@ -32,4 +33,38 @@ bool initEngine(sf::RenderWindow& window) {
     }
     //window.draw(text);
     return 1;
+}
+
+Engine::Engine(sf::RenderWindow& window) {
+    sf::Thread thread([&] {
+        sf::Font font;
+        #ifdef _WIN32
+        if (!font.loadFromFile("C:/Windows/Fonts/comicbd.ttf")) {
+            std::cerr << "[pbengine] cannot load comicbd.ttf (Windows)" << std::endl;
+        }
+        #endif
+        #ifdef linux
+        if (!font.loadFromFile("/usr/share/fonts/truetype/msttcorefonts/comicbd.ttf")) {
+            std::cerr << "[pbengine] cannot load comicbd.ttf (Linux)" << std::endl;
+        }
+        #endif
+        sf::Text text;
+        text.setFont(font);
+        text.setString("Hello World");
+        text.setCharacterSize(24);
+        text.setFillColor(sf::Color::White);
+        text.setStyle(sf::Text::Underlined);
+        while (window.isOpen()) {
+            sf::Event event;
+            while (window.pollEvent(event)) {
+                if (event.type == sf::Event::Closed)
+                    window.close();
+            }
+            window.clear();
+            window.draw(text);
+            window.display();
+        }
+        //window.draw(text);
+    });
+    thread.launch();
 }
--- pbengine/pbengine.hpp
+++ pbengine/pbengine.hpp
@@ -6,35 +6,6 @@
 bool initEngine(sf::RenderWindow& window);
 class Engine {
 public:
-    Engine(sf::RenderWindow& window) {
-        sf::Font font;
-        #ifdef _WIN32
-        if (!font.loadFromFile("C:/Windows/Fonts/comicbd.ttf")) {
-            std::cerr << "[pbengine] cannot load comicbd.ttf (Windows)" << std::endl;
-        }
-        #endif
-        #ifdef linux
-        if (!font.loadFromFile("/usr/share/fonts/truetype/msttcorefonts/comicbd.ttf")) {
-            std::cerr << "[pbengine] cannot load comicbd.ttf (Linux)" << std::endl;
-        }
-        #endif
-        sf::Text text;
-        text.setFont(font);
-        text.setString("Hello World");
-        text.setCharacterSize(24);
-        text.setFillColor(sf::Color::White);
-        text.setStyle(sf::Text::Underlined);
-        while (window.isOpen()) {
-            sf::Event event;
-            while (window.pollEvent(event)) {
-                if (event.type == sf::Event::Closed)
-                    window.close();
-            }
-            window.clear();
-            window.draw(text);
-            window.display();
-        }
-        //window.draw(text);
-    }
+    Engine(sf::RenderWindow& window);
 };
 #endif

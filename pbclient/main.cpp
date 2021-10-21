2672367da7e226381aac4da8cd511130e804e142
Luca Apolzan <jaguar3217@gmail.com>
2021-10-21 17:48:43 +0300

    Add window.setActive() to fix segfault.

--- pbclient/main.cpp
+++ pbclient/main.cpp
@@ -5,9 +5,11 @@
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(512, 256), "Pixelbuild");
+    window.setActive(false);
     //if (!initEngine(window)) {
     //    std::cerr << "Engine Error" << std::endl;
     //}
     Engine engine(window);
+    window.close();
     return 0;
 }

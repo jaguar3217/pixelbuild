3e2fc6847a2d44523c52a78404d97246de3c2654
Luca Apolzan <jaguar3217@gmail.com>
2021-11-11 08:30:44 +0200

    BUG: White square, Sprite positioned on top left.

--- pbengine/Player.cpp
+++ pbengine/Player.cpp
@@ -27,4 +27,5 @@ void Player::draw(sf::RenderTarget& target, sf::RenderStates states) const
 {
     states.transform *= getTransform();
     states.texture = m_sprite.getTexture();
+    target.draw(m_sprite, states);
 }

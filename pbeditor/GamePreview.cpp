3b9897431f33ab5148a60911e417f6c61e13398c
Luca Apolzan <jaguar3217@gmail.com>
2022-11-10 16:57:29 +0200

    Integrate engine into editor.

--- pbeditor/GamePreview.cpp
+++ pbeditor/GamePreview.cpp
@@ -5,6 +5,10 @@ void GamePreview::OnUpdate()
     // Clear the view
     clear(sf::Color(255, 0, 0));
 
-    // Draw the circle
+    // Render and draw engine output
+    m_engine.Render();
+
+    // Draw the circle & engine render output
     draw(rectangle);
+    draw(m_sprite);
 }
--- pbeditor/GamePreview.hpp
+++ pbeditor/GamePreview.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "wxSFMLCanvas.hpp"
+#include "pbengine.hpp"
 
 class GamePreview : public wxSFMLCanvas
 {
@@ -15,10 +16,26 @@ public:
         // set up the circle
         rectangle.setSize(sf::Vector2f(512.f, 256.f));
         rectangle.setFillColor(sf::Color::Yellow);
+
+        // set up the texture
+        if (!m_texture.create(512, 256, true))
+        {
+            std::cerr << "[pbeditor] Cannot create sf::RenderTexture\n";
+        }
+
+        // set up the sprite
+        m_sprite.setTexture(engineOut);
+
+        // configure the engine
+        m_engine.SetTex(&m_texture);
     }
 
 private:
     virtual void OnUpdate();
 
     sf::RectangleShape rectangle;
+    sf::RenderTexture m_texture;
+    const sf::Texture& engineOut = m_texture.getTexture();
+    sf::Sprite m_sprite;
+    Engine m_engine;
 };
